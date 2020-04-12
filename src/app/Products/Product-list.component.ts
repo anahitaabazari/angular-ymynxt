a2167826bd16102be397bc7f065b12de6f9d6581
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-12 11:55:56 -0700

    1 file modified

--- src/app/Products/Product-list.component.ts
+++ src/app/Products/Product-list.component.ts
@@ -1,1 +1,9 @@
 import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './Product-list.component.html'})
+
+  export class ProductlistComponent{
+    pagetilet : string ="test Product list";
+  }
