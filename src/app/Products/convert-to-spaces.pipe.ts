3c993a5a21a9fb8b1f295ac29aa5d4e65fc78863
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-13 15:26:43 -0700

    2 files modified

---
+++ src/app/Products/convert-to-spaces.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({
+  name : 'ConvertToSpacesPipe'
+})
+
+export class ConvertToSpacesPipe implements PipeTransform 
+{
+ transfrom(value: string, character:string) : string {
+
+ }
+
+ 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductlistComponent } from './Products/Product-list.component';
+import { ConvertToSpacesPipe } from './Products/convert-to-spaces.pipe';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, ProductlistComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductlistComponent , ConvertToSpacesPipe ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
