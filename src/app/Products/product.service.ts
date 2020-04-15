fb3aac0173de6abe463783b68d289e60495b0c2f
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-15 15:34:02 -0700

    1 file modified

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -5,4 +5,27 @@ import { IProduct } from './pp';
 
 export class ProductService{
 
+
+GetProducts() : IProduct[] =[
+      {
+      "productId" : 1,
+      "productName" : "name-1",
+      "productRate" : 1,
+      },
+       {
+      "productId" : 2,
+      "productName" : "NNN-ame2222",
+      "productRate" : 2,
+      },
+       {
+      "productId" : 3,
+      "productName" : "NNN-333333",
+      "productRate":1,
+      },
+       {
+      "productId" : 4,
+      "productName" : "NNN-444444",
+      "productRate":3,
+      }
+    ]
 }
