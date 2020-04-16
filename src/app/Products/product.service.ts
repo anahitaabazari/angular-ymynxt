762f716302d98d51ed84751745138dff342ffa52
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-16 15:47:25 -0700

    1 file modified

--- src/app/Products/product.service.ts
+++ src/app/Products/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { IProduct } from './pp';
-//import { HttpClient } from '@angular/common/http';
+
 
 
 @Injectable({
