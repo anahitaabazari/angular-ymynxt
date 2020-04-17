9ee5b41d0deec25069ec6158bfde052a4bd4a73b
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-16 17:10:27 -0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductlistComponent } from './Products/Product-list.component';
+import { productdtl  } from         './Products/Product-dtl.component';
 import { ConvertToSpacesPipe } from './Products/convert-to-spaces.pipe';
 import { StartComponent } from './Shared/star.component';
 
@@ -14,7 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule , HttpClientModule],
-  declarations: [ AppComponent, HelloComponent, ProductlistComponent , ConvertToSpacesPipe, StartComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductlistComponent , ConvertToSpacesPipe, StartComponent , productdtl  ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService],
 
