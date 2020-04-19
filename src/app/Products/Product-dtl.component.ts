682415859200f9e16b9421e0c445e17a7975d287
anahitaabazari <anahita_abazari@yahoo.com>
2020-04-18 22:04:00 -0700

    2 files modified

--- src/app/Products/Product-dtl.component.ts
+++ src/app/Products/Product-dtl.component.ts
@@ -1,6 +1,7 @@
 import {Component ,OnInit } from '@angular/core'
 import {IProduct } from './pp'
 import { ActivatedRoute } from '@angular/router'
+import { Router } from '@angular/router'
 
 @Component({
       selector: 'pm-products-detail',
